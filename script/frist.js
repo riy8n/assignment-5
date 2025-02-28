ec3aa6284ab9f69679728c836c5e751f1b9fcda4
riy8n <mahafujhossainriyan02@gmail.com>
2025-03-01 01:53:30 +0600

    first commit

---
+++ script/frist.js
@@ -0,0 +1,10 @@
+const comBtn= document.querySelectorAll(".btn-complete")
+for(let i=0; i<comBtn.length; i++){
+    const completeBtn=comBtn[i];
+    console.log(completeBtn)
+
+}
+
+//   const naValue=getInnerTextById("nav-value")
+//     const cardValue=getInnerTextById("card-value");
+//     console.log(naValue,cardValue)
---
+++ script/toggle.js
@@ -0,0 +1,12 @@
+function getInnerTextById(id){
+    const value=document.getElementById(id).innerText;
+    const convertedValue=parseFloat(value);
+console.log(typeof convertedValue)
+
+}
+// function getInnerTextById(id) {
+//     const value = document.getElementById(id).innerText;
+//     const convertedValue = parseFloat(value);
+//     return convertedValue;
+
+// }
